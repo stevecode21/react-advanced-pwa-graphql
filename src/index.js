b974d1fb1d1c97b635959d754122e44629b21461
Steve Suarez <suarezsteve21@gmail.com>
2020-04-27 16:49:54 -0500

    2. Preparando el entorno de desarrollo

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//  Renderizaré mi JSX en el nodo app que es un div que crearé en mi html para renderizar toda mi aplicación hecha en React
+ReactDOM.render(<h1>Hola Steve</h1>, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  output: {
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
